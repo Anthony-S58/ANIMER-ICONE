6382210b538ad2e8679f24f4bbf325565994bc01
Anthony-S58 <tonysim01@hotmail.com>
2021-07-01 10:27:33 +0200

    défi terminé

---
+++ main.js
@@ -0,0 +1,56 @@
+function balanceScale() {
+    // Développez la fonction
+    let balance = document.getElementById('balanceIcon');
+    let lock = document.getElementById('lockIcon');
+    let fleche = document.getElementById('flecheIcon');
+
+   
+
+
+setTimeout(function(){ balance.innerHTML="&#xf24e"; }, 1000);
+setTimeout(function(){ balance.innerHTML="&#xf515"; }, 2000);
+setTimeout(function(){ balance.innerHTML="&#xf24e"; }, 3000);              
+setTimeout(function(){ balance.innerHTML="&#xf516"; }, 4000);
+
+    
+
+setTimeout(function(){ lock.innerHTML="&#xf023"; }, 1000);
+setTimeout(function(){ lock.innerHTML="&#xf09c"; }, 2000);
+setTimeout(function(){ lock.innerHTML="&#xf023"; }, 3000);
+setTimeout(function(){ lock.innerHTML="&#xf09c"; }, 4000);
+
+setTimeout(function(){ fleche.innerHTML="&#xf109"; }, 1000);
+setTimeout(function(){ fleche.innerHTML="&#xf5fc"; }, 2000);
+setTimeout(function(){ fleche.innerHTML="&#xf109"; }, 3000);
+setTimeout(function(){ fleche.innerHTML="&#xf5fc"; }, 4000);
+
+   
+
+    }
+    balanceScale();
+    setInterval(balanceScale, 4000);
+
+
+    function phoneScale() {
+        // Développez la fonction
+        let batterie = document.getElementById('batterieIcon');
+        
+       
+    
+    
+    setTimeout(function(){ batterie.innerHTML="&#xf244"; }, 1000);
+    setTimeout(function(){ batterie.innerHTML="&#xf243"; }, 2000);
+    setTimeout(function(){ batterie.innerHTML="&#xf242"; }, 3000);              
+    setTimeout(function(){ batterie.innerHTML="&#xf241"; }, 4000);
+    setTimeout(function(){ batterie.innerHTML="&#xf240"; }, 5000);
+    
+    
+       
+    
+        }
+        phoneScale();
+        setInterval(phoneScale, 5000);
+    
+
+
+  
